69494b48d405f42c99f18f991014745c2cb73106
leipengcheng <leipc@xiaoshouyi.com>
2018-08-10 10:10:54 +0800

    add canal first

---
+++ driver/src/main/java/com/alibaba/otter/canal/parse/driver/mysql/socket/SocketChannel.java
@@ -0,0 +1,23 @@
+package com.alibaba.otter.canal.parse.driver.mysql.socket;
+
+import java.io.IOException;
+import java.net.SocketAddress;
+
+/**
+ * @author agapple 2018年3月12日 下午10:36:44
+ * @since 1.0.26
+ */
+public interface SocketChannel {
+
+    public void write(byte[]... buf) throws IOException;
+
+    public byte[] read(int readSize) throws IOException;
+
+    public byte[] read(int readSize, int timeout) throws IOException;
+
+    public boolean isConnected();
+
+    public SocketAddress getRemoteSocketAddress();
+
+    public void close();
+}
